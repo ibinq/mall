f914ac6e843f424deda14f4ddc0e35d5bd33071b
ZhouBing <5118757+ZhouBing007@user.noreply.gitee.com>
2020-02-28 16:33:06 +0800

    add user

--- src/components/user/Users.vue
+++ src/components/user/Users.vue
@@ -5,32 +5,225 @@
       <el-breadcrumb-item>基本信息</el-breadcrumb-item>
       <el-breadcrumb-item>账户管理</el-breadcrumb-item>
     </el-breadcrumb>
-    <div class="user-list">
-      <div style="margin-top: 15px;">
-        <el-input placeholder="请输入内容" v-model="input3" class="input-with-select" style="width:50%">
-          <el-button slot="append" icon="el-icon-search"></el-button>
-        </el-input>
-        <el-button type="primary" class="addBtn">添加用户</el-button>
-      </div>
-      <el-table :data="tableData" border style="width: 100%">
-        <el-table-column prop="date" label="日期" width="180"></el-table-column>
-        <el-table-column prop="name" label="姓名" width="180"></el-table-column>
-        <el-table-column prop="address" label="地址"></el-table-column>
+    <el-card class="user-box-card">
+      <el-row :gutter="20">
+        <el-col :span="10">
+          <el-input placeholder="请输入内容" v-model="queryInfo.query">
+            <el-button slot="append" icon="el-icon-search" @click="search()"></el-button>
+          </el-input>
+        </el-col>
+        <el-col :span="4">
+          <el-button type="primary" @click="dialogFormVisible = true">添加用户</el-button>
+        </el-col>
+      </el-row>
+
+      <el-table class="user-table-list" :data="userList" :stripe="true" :border="true">
+        <el-table-column type="index"></el-table-column>
+        <el-table-column label="姓名" prop="username"></el-table-column>
+        <el-table-column label="邮箱" prop="email"></el-table-column>
+        <el-table-column label="电话" prop="phone"></el-table-column>
+        <el-table-column label="状态" prop="deleted">
+          <template slot-scope="scope">
+            <el-switch v-model="scope.row.status" @change="changStatus(scope.row)"></el-switch>
+          </template>
+        </el-table-column>
+        <el-table-column label="操作">
+          <template slot-scope="scope">
+            <el-tooltip class="item" effect="dark" content="编辑" placement="top" :enterable="false">
+              <el-button type="primary" icon="el-icon-edit" circle></el-button>
+            </el-tooltip>
+            <el-tooltip class="item" effect="dark" content="删除" placement="top" :enterable="false">
+              <el-button type="danger" icon="el-icon-delete" circle @click="open(scope.row.id)"></el-button>
+            </el-tooltip>
+            <el-tooltip
+              class="item"
+              effect="dark"
+              content="分配角色"
+              placement="top"
+              :enterable="false"
+            >
+              <el-button type="warning" icon="el-icon-setting" circle></el-button>
+            </el-tooltip>
+          </template>
+        </el-table-column>
       </el-table>
-    </div>
+
+      <el-pagination
+        class="user-page"
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="queryInfo.pageNum"
+        :page-sizes="[2, 4, 6, 8]"
+        :page-size="queryInfo.pageSize"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="total"
+      ></el-pagination>
+    </el-card>
+
+    <el-dialog title="添加用户" :visible.sync="dialogFormVisible">
+      <el-form
+        ref="addUserFormRef"
+        :model="addUserFrom"
+        :rules="addUserFormRules"
+        label-width="80px"
+      >
+        <el-form-item label="用户名" prop="username">
+          <el-input v-model="addUserFrom.username" placeholder="请输入用户名"></el-input>
+        </el-form-item>
+        <el-form-item label="密码" prop="password">
+          <el-input v-model="addUserFrom.password" placeholder="请输入密码" type="password"></el-input>
+        </el-form-item>
+        <el-form-item label="邮箱" prop="email">
+          <el-input v-model="addUserFrom.email" placeholder="请输入邮箱"></el-input>
+        </el-form-item>
+        <el-form-item label="电话" prop="phone">
+          <el-input v-model="addUserFrom.phone" placeholder="请输入电话"></el-input>
+        </el-form-item>
+        <!-- <el-form-item label="头像">
+          <el-input v-model="input" placeholder="请输入头像"></el-input>
+        </el-form-item>-->
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisible = false">取 消</el-button>
+        <el-button type="primary" @click="dialogFormVisible = false">确 定</el-button>
+      </div>
+    </el-dialog>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    
+       // 验证邮箱的规则
+    var checkEmail = (rule, value, cb) => {
+      // 验证邮箱的正则表达式
+      const regEmail = /^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+(\.[a-zA-Z0-9_-])+/
+      if (regEmail.test(value)) {
+        // 合法的邮箱
+        return cb()
+      }
+      cb(new Error('请输入合法的邮箱'))
+    };
+    // 验证手机号的规则
+    var checkMobile = (rule, value, cb) => {
+      // 验证手机号的正则表达式
+      const regMobile = /^(0|86|17951)?(13[0-9]|15[012356789]|17[678]|18[0-9]|14[57])[0-9]{8}$/
+      if (regMobile.test(value)) {
+        return cb()
+      }
+      cb(new Error('请输入合法的手机号'))
+    }
+
+    return {
+      queryInfo: {
+        query: "",
+        pageSize: 2,
+        pageNum: 1
+      },
+      userList: [],
+      total: 0,
+      dialogFormVisible: false,
+      addUserFrom:{
+        username:'',
+        password:'',
+        email:'',
+        phone:''
+      },
+
+
+      addUserFormRules:{
+        username: [
+          { required: true, message: "请输入用户名", trigger: "blur" },
+          { min: 3, max: 10, message: "长度在 3 到 10 个字符", trigger: "blur" }
+        ],
+        password: [
+          { required: true, message: "请输入密码", trigger: "change" },
+          { min: 3, max: 16, message: "长度在 3 到 16 个字符", trigger: "blur" }
+        ],
+         email: [
+          { required: true, message: "请输入正确的邮箱地址", trigger: "change" },
+           {validator:checkEmail,trigger:'blur'}
+        ],
+        phone: [
+          { required: true, message: "请输入正确的手机号码", trigger: "change" },
+           {validator:checkMobile,trigger:'blur'}
+        ]
+      }
+    };
+  },
+  created() {
+    this.getUserList();
+  },
+  methods: {
+    search() {
+      this.getUserList();
+    },
+    open(id) {
+      console.log(id);
+      this.$confirm("是否删除此用户?", "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning"
+      })
+        .then(async () => {
+          const res = await this.$http.post(
+            "/user/delete",
+            this.$qs.stringify({ id: id })
+          );
+          if (res.data.data.code !== 0) return this.$message.error("删除失败");
+          this.$message({
+            type: "success",
+            message: "删除成功!"
+          });
+        })
+        .catch(() => {
+          this.$message({
+            type: "info",
+            message: "已取消删除"
+          });
+        });
+    },
+    handleSizeChange(newPageSize) {
+      this.queryInfo.pageSize = newPageSize;
+      this.getUserList();
+    },
+    handleCurrentChange(newPageNum) {
+      this.queryInfo.pageNum = newPageNum;
+      this.getUserList();
+    },
+
+    async changStatus(user) {
+      const res = await this.$http.post("user/update", user);
+      if(res.data.code !== 0) return this.$message.error('操作失败');
+      if(user.status === true) return this.$message.success('账户已启用');
+      return this.$message.error('账户已停用');
+
+      
+      //console.log(res);
+    },
+    async getUserList() {
+      const res = await this.$http.get("user/list", {
+        params: this.queryInfo
+      });
+      if (res.data.code !== 0) {
+        return this.$message.error("获取用户数据失败！");
+      }
+      this.userList = res.data.data.users;
+      this.total = res.data.data.total;
+    }
+  }
+};
 </script>
 
 <style   scoped>
-.addBtn {
-  margin-left: 20px;
+.user-page {
+  margin-top: 15px;
 }
-.user-list {
-  background-color: #fff;
-  box-shadow: 1px  1px 1px #b49f9f;
+.user-table-list {
+  margin-top: 15px;
+}
+.user-box-card {
+  margin-top: 15px;
 }
 </style>
